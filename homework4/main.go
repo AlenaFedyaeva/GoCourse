425a829d4c090f5d7659fa160f94262bd68bfc88
Alena <291407@gmail.com>
2020-05-09 22:40:09 +0300

    Task 4 complted

--- homework4/main.go
+++ homework4/main.go
@@ -1,95 +1,21 @@
 package main
 
 import (
-	"GoCource/homework4/task1"
-	"GoCource/homework4/task2"
-	"GoCource/homework4/task3"
+	// "GoCource/homework4/task1"
+	// "GoCource/homework4/task2"
+	// "GoCource/homework4/task3"
 	"GoCource/homework4/task4"
 	"fmt"
 )
 
-const boardLen int = 8
 
-type Board struct {
-	board [boardLen][boardLen]int
-	posI int
-	posJ int
-}
-
-var board Board
-var possibleMoves [][]int
-func init ()  {
-	fmt.Println("Homework 4")
-	board.setPos(0,0)
-	possibleMoves=[][]int{{-1,-2},{-2, -1},{-2,  1},{ 1, -2},{-1,  2},{ 2,-1},{ 1, 2},{ 2, 1},}	
-	//fmt.Print(possibleMoves)
-}
-func (b *Board) checkPos(i,j int) (bool){
-	li:=len(b.board)
-	lj:=len(b.board[0])
-	if((i>=0 && i<li)&&(j>=0 && j<lj)){
-		return true	
-	}
-	return false
-}
-
-func (b *Board) getMoves() ([][]int) {
-	var arrMoves [][]int 
-	for _, moves := range possibleMoves{
-		i:=b.posI+moves[0]
-		j:=b.posJ+moves[1]
-		if(b.checkPos(i,j) && b.board[i][j]==0){
-			arrMoves=append(arrMoves,[]int{i,j})
-			//fmt.Println(i,j)
-		}
-	}
-	fmt.Println("Can move to:",arrMoves)
-	return arrMoves
-}
-
-func (b *Board) getPos() (int,int){
-	fmt.Println("Position on board: ",b.posI,b.posJ)
-	return b.posI,b.posJ
-}
-func (b *Board) printBoard() (){
-	for _, arr := range b.board {
-		fmt.Println(arr)
-	}
-}
-
-func (b *Board) setPos(i,j int) (bool){
-	li:=len(b.board)
-	lj:=len(b.board[0])
-	if(i<li && j<lj){
-		b.board[i][i]=1
-		b.posI=i
-		b.posJ=j
-		fmt.Println("Position on board changed to: ",b.posI,b.posJ)
-	} else{
-		return false
-	}
-	b.printBoard()
-	return true
-}
-
-
-func Task4() {
-	fmt.Println("Task 4")
-	board.getPos()
-	for i := 0; i < 5; i++ {
-		arr:=board.getMoves()
-		if(len(arr)>0){
-			board.setPos(arr[0][0],arr[0][1])
-		}
-	}
-	board.getMoves() 
-
-	
-}
 func main() {
-	task1.Task1()
-	task2.Task2()
-	task3.Task3()
+	
+	// task1.Task1()
+	// task2.Task2()
+	// task3.Task3()
 	task4.Task4()
+
 	fmt.Println("\nThe end")
+	
 }
--- homework4/task4/chess.go
+++ homework4/task4/chess.go
@@ -1,83 +1,107 @@
 package task4
 import (
 	"fmt"
+	"math/rand"
 )
 
 const boardLen int = 8
+//board -  доска
+var board Board
+//posibloPints -  - массив, содержащий значения точек для вычисления возможных движений коня на доске
+var possiblePoints []Point
 
+//Point - точка на шахматной доске
+type Point struct{
+	x int
+	y int
+}
+func (p *Point) setValue(in Point)  {
+	p.x=in.x
+	p.y=in.y
+}
+//Board - шахматная доска 8*8
 type Board struct {
 	board [boardLen][boardLen]int
-	posI int
-	posJ int
+	Point
 }
 
-var board Board
-var possibleMoves [][]int
-func init ()  {
-	fmt.Println("Homework 4")
-	board.setPos(0,0)
-	possibleMoves=[][]int{{-1,-2},{-2, -1},{-2,  1},{ 1, -2},{-1,  2},{ 2,-1},{ 1, 2},{ 2, 1},}	
-	//fmt.Print(possibleMoves)
-}
+
+//checkPos - функция, проверяющая находится ли позиция на доске
 func (b *Board) checkPos(i,j int) (bool){
-	li:=len(b.board)
-	lj:=len(b.board[0])
+	lj:=len(b.board)
+	li:=len(b.board[0])
 	if((i>=0 && i<li)&&(j>=0 && j<lj)){
 		return true	
 	}
 	return false
 }
 
-func (b *Board) getMoves() ([][]int) {
-	var arrMoves [][]int 
-	for _, moves := range possibleMoves{
-		i:=b.posI+moves[0]
-		j:=b.posJ+moves[1]
-		if(b.checkPos(i,j) && b.board[i][j]==0){
-			arrMoves=append(arrMoves,[]int{i,j})
-			//fmt.Println(i,j)
+//getPoints - возвращает массив из точек, в которые конь сможет сделать ход
+//Проверяем находтся ли позиция на доске 
+func (b *Board) getPoints() ([]Point) {
+	var arr []Point 
+	for _, pt := range possiblePoints{
+		x:=b.x+pt.x
+		y:=b.y+pt.y
+		if(b.checkPos(x,y)){
+			arr=append(arr,Point{x,y})
 		}
 	}
-	fmt.Println("Can move to:",arrMoves)
-	return arrMoves
+	fmt.Println("Can move to points:",arr)
+	return arr
 }
 
-func (b *Board) getPos() (int,int){
-	fmt.Println("Position on board: ",b.posI,b.posJ)
-	return b.posI,b.posJ
+func (b *Board) getPos() (Point){
+	fmt.Println("Position on board: ",b.x,b.y)
+	return Point{b.x,b.y}
 }
+
 func (b *Board) printBoard() (){
-	for _, arr := range b.board {
-		fmt.Println(arr)
+	for i := 0; i < len(b.board[0])*2+3; i++ {
+		if(i==0){
+			fmt.Print("   ")
+		}
+		fmt.Print("-")
 	}
+	fmt.Print("x\n")
+	for j, arr := range b.board {
+		fmt.Println(j,"|",arr)
+	}
+	fmt.Println("  y ")
 }
-
-func (b *Board) setPos(i,j int) (bool){
-	li:=len(b.board)
-	lj:=len(b.board[0])
-	if(i<li && j<lj){
-		b.board[i][i]=1
-		b.posI=i
-		b.posJ=j
-		fmt.Println("Position on board changed to: ",b.posI,b.posJ)
+//setPoint - выставляет позицию коня на доску, в декартовых координатах (x,y). 
+// Х - по горизонтали, У- по вертикали
+//Начало координат (0,0) в левом верхнем углу доски
+func (b *Board) setPoint(p Point) (bool){
+	lj:=len(b.board)
+	li:=len(b.board[0])
+	if(p.x<li && p.y<lj){
+		b.board[p.y][p.x]=1
+		b.Point.setValue(p)
+		fmt.Println("Position on board changed to point: ",b.Point)
 	} else{
 		return false
 	}
-	b.printBoard()
 	return true
 }
 
 
-func Task4() {
+func init ()  {
 	fmt.Println("Task 4")
-	board.getPos()
+	board.setPoint(Point{0,0})
+	possiblePoints=[]Point{{-1,-2},{-2, -1},{-2,  1},{ 1, -2},{-1,  2},{ 2,-1},{ 1, 2},{ 2, 1},}
+}
+
+//Task4 - задание 4
+func Task4() {
+	arr:=board.getPoints()
+	board.setPoint(arr[0])
 	for i := 0; i < 5; i++ {
-		arr:=board.getMoves()
+		arr:=board.getPoints()
 		if(len(arr)>0){
-			board.setPos(arr[0][0],arr[0][1])
+			rnd:=rand.Intn(len(arr))
+			board.setPoint(arr[rnd])
+			board.printBoard()
 		}
 	}
-	board.getMoves() 
-
-	
 }
