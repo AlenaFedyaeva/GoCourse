d48e8bc4b412991890057df3e9bda6b4bd415372
Alena <291407@gmail.com>
2020-05-09 22:48:38 +0300

    fix comments

--- homework4/main.go
+++ homework4/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	// "GoCource/homework4/task1"
-	// "GoCource/homework4/task2"
-	// "GoCource/homework4/task3"
+	"GoCource/homework4/task1"
+	"GoCource/homework4/task2"
+	"GoCource/homework4/task3"
 	"GoCource/homework4/task4"
 	"fmt"
 )
@@ -11,9 +11,9 @@ import (
 
 func main() {
 	
-	// task1.Task1()
-	// task2.Task2()
-	// task3.Task3()
+	task1.Task1()
+	task2.Task2()
+	task3.Task3()
 	task4.Task4()
 
 	fmt.Println("\nThe end")
