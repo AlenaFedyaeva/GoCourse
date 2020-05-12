414e324ed9cba72f81451077f0f13049842016b3
Alena <291407@gmail.com>
2020-05-12 15:44:05 +0300

    Task5 completed

--- homework5/main.go
+++ homework5/main.go
@@ -7,21 +7,6 @@ import (
 	"fmt"
 )
 
-func Extend(slice []int, element int) []int {
-	if cap(slice) == len(slice) {
-		fmt.Println("slice is full!", element)
-		newSlice := make([]int, len(slice)+1) //, 2*cap(slice))
-		copy(newSlice, slice)                 //копируем старые значения
-		newSlice[len(slice)] = element        //добавляем новое
-		slice = newSlice                      //заменяем слайс на новый
-		return slice
-	}
-	n := len(slice)
-	slice = slice[0 : n+1]
-	slice[n] = element
-	return slice
-}
-
 
 func main() {
 	task2.Task2()
