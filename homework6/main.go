327dd6fa09444a1e56f2936768c8c3a3534ff8b4
Alena <291407@gmail.com>
2020-05-14 17:14:10 +0300

    Completed task2

--- homework6/main.go
+++ homework6/main.go
@@ -1,28 +1,55 @@
 package main
 
 import (
+	"GoCource/homework6/task2"
 	"fmt"
 	"image"
 	"image/color"
-	"image/draw"
 	"image/png"
 	"log"
 	"os"
 )
 
+func ChessBoard() {
+	width := 800
+	height := 800
+
+	upLeft := image.Point{0, 0}
+	lowRight := image.Point{width, height}
+
+	img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+
+	// Colors are defined by Red, Green, Blue, Alpha uint8 values.
+	// cyan := color.RGBA{100, 200, 200, 0xff}
+
+	// Set color for each pixel.
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			switch {
+			case x < width/2 && y < height/2: // upper left quadrant
+				img.Set(x, y, color.Black)
+			case x >= width/2 && y >= height/2: // lower right quadrant
+				img.Set(x, y, color.White)
+			default:
+				// Use zero value.
+			}
+		}
+	}
+
+	// Encode as PNG.
+	f,err := os.Create("image.png")
+	if err != nil {
+		log.Fatalf("Failed create file: %s", err)
+	}
+	defer f.Close()
+	png.Encode(f, img)
+}
+
+
 
 func main() {
-	green := color.RGBA{0, 255, 0, 255}
-    rectImg := image.NewRGBA(image.Rect(0, 0, 200, 200))
+	task2.DrawLines()
 
-    draw.Draw(rectImg, rectImg.Bounds(), &image.Uniform{green}, image.ZP, draw.Src)
-
-    file, err := os.Create("rectangle.png")
-    if err != nil {
-        log.Fatalf("Failed create file: %s", err)
-    }
-       defer file.Close()
-    png.Encode(file, rectImg)
-
-	fmt.Println("\nThe end")	
+	// ChessBoard()
+	fmt.Println("\nThe end")
 }
