4fe59c6a646492676975aa9620f6e76506e8eaef
Alena <291407@gmail.com>
2020-05-14 15:33:14 +0300

    Add usefull info

--- homework6/main.go
+++ homework6/main.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"log"
+	"os"
 )
 
 
 func main() {
+	green := color.RGBA{0, 255, 0, 255}
+    rectImg := image.NewRGBA(image.Rect(0, 0, 200, 200))
+
+    draw.Draw(rectImg, rectImg.Bounds(), &image.Uniform{green}, image.ZP, draw.Src)
+
+    file, err := os.Create("rectangle.png")
+    if err != nil {
+        log.Fatalf("Failed create file: %s", err)
+    }
+       defer file.Close()
+    png.Encode(file, rectImg)
 
 	fmt.Println("\nThe end")	
 }
