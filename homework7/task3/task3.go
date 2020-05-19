55ebf9cc5f2258de6c7f98fb491ece12b69fdb5c
Alena <291407@gmail.com>
2020-05-19 08:25:00 +0300

    Task3 fixed

--- homework7/task3/task3.go
+++ homework7/task3/task3.go
@@ -5,33 +5,23 @@ import (
 	"io"
 	"log"
 	"net"
-	"sync"
 	"time"
 )
 
 func handleConn(c net.Conn, quit chan struct{}) {
 	defer c.Close()
 	for {
-		select {
-		case <-quit:
-			fmt.Println("hBYE")
-			quit <-struct{}{} 
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n\r"))
+		fmt.Println("time", time.Now().Format("15:04:05\n\r"))
+		if err != nil {
 			return
-		default:
-			fmt.Println("hdefault")
-			_, err := io.WriteString(c, time.Now().Format("15:04:05\n\r"))
-			fmt.Println("time", time.Now().Format("15:04:05\n\r"))
-			if err != nil {
-				return
-			}
-			time.Sleep(1 * time.Second)
 		}
+		time.Sleep(1 * time.Second)
 
 	}
 }
 
 func exit(quit chan struct{}) { //, wg *sync.WaitGroup) {
-	// defer wg.Done()
 	for {
 		str := ""
 		fmt.Println("Для выхода наберите exit и нажмите кнопку Enter")
@@ -43,39 +33,25 @@ func exit(quit chan struct{}) { //, wg *sync.WaitGroup) {
 		}
 	}
 }
-func tcpServer(quit chan struct{}, wg *sync.WaitGroup) {
-	defer wg.Done()
+func tcpServer(quit chan struct{}) {
 	listener, err := net.Listen("tcp", "localhost:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
 	for {
-		select {
-		case <-quit:
-			fmt.Println("BYE all")
-			close(quit)
-			return
-		default:
-			fmt.Println("default")
-			conn, err := listener.Accept()
-			if err != nil {
-				log.Print(err)
-				continue
-			}
-			handleConn(conn, quit)
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
 		}
-
+		handleConn(conn, quit)
 	}
 }
 
 func main() {
-
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
 	quit := make(chan struct{})
 	defer close(quit)
 	go exit(quit)
-	go tcpServer(quit, wg)
-
-	wg.Wait()
+	go tcpServer(quit)
+	<-quit
 }
